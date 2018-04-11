4aeb0d7d6fc619c092d82c93bf604ee0ae2cfed2
vshev4enko <vshev4enko@vshev4enko-MacBook-Pro.local>
2018-04-11 11:05:35 +0300

    refactoring cache_server, bahaviour gen_server

---
+++ cache_server/Makefile
@@ -0,0 +1,5 @@
+PROJECT = cache_server
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+
+include erlang.mk
--- cache_server/src/cache_server.erl
+++ cache_server/src/cache_server.erl
@@ -18,68 +18,48 @@
   lookup/1,
   lookup_by_date/2,
   stop/0,
-  async_insert/3]).
+  handle_call/3,
+  handle_cast/2]).
 -export([
   init/1,
-  handle_call/3,
-  handle_cast/2,
   handle_info/2,
   terminate/2,
   code_change/3]).
-
 
 start_link(Args) ->
   TTL = proplists:get_value(drop_interval, Args),
   gen_server:start_link({local, ?MODULE}, ?MODULE, TTL, []).
 
-insert(Key, Value, TTL) ->
-  gen_server:call(?MODULE, {insert, Key, Value, TTL}).
-
-async_insert(Key, Value, TTL) ->
-  gen_server:cast(?MODULE, {insert, Key, Value, TTL}).
-
-lookup(Key) ->
-  gen_server:call(?MODULE, {lookup, Key}).
-
-lookup_by_date(DateFrom, DateTo) ->
-  gen_server:call(?MODULE, {lookup_by_date, DateFrom, DateTo}).
-
 stop() ->
   gen_server:stop(?MODULE).
 
 init(TTL) ->
-  Tab = ets:new(?MODULE, [named_table]),
+  Tab = ets:new(?MODULE, [named_table, public]),
   timer:send_interval(TTL * 1000, self(), drop_evaluate),
   {ok, {Tab, TTL}}.
 
-handle_call({insert, Key, Value, TTL}, _, State) ->
+insert(Key, Value, TTL) ->
   StartDate = calendar:universal_time(),
   DateSeconds = calendar:datetime_to_gregorian_seconds(StartDate),
   AllSeconds = DateSeconds + TTL,
   EndDate = calendar:gregorian_seconds_to_datetime(AllSeconds),
   ets:insert(?MODULE, {Key, Value, StartDate, EndDate}),
-  {reply, ok, State};
-handle_call({lookup, Key}, _, State) ->
+  ok.
+
+lookup(Key) ->
   case ets:lookup(?MODULE, Key) of
     [{Key, Value, _, _}] ->
-      {reply, {ok, Value}, State};
+      {ok, Value};
     _ = Result ->
-      {reply, {ok, Result}, State}
-  end;
-handle_call({lookup_by_date, DateFrom, DateTo}, _, State) ->
+      Result
+  end.
+
+lookup_by_date(DateFrom, DateTo) ->
   MS = ets:fun2ms(fun({Key, Value, Date, _})
     when Date >= DateFrom andalso Date =< DateTo ->
     {Key, Value} end),
-  Reply = {ok, ets:select(?MODULE, MS)},
-  {reply, Reply, State}.
-
-handle_cast({insert, Key, Value, TTL}, State) ->
-  StartDate = calendar:universal_time(),
-  DateSeconds = calendar:datetime_to_gregorian_seconds(StartDate),
-  AllSeconds = DateSeconds + TTL,
-  EndDate = calendar:gregorian_seconds_to_datetime(AllSeconds),
-  ets:insert(?MODULE, {Key, Value, StartDate, EndDate}),
-  {noreply, State}.
+  Value = {ok, ets:select(?MODULE, MS)},
+  {ok, Value}.
 
 handle_info(drop_evaluate, State) ->
   FirstKey = ets:first(?MODULE),
@@ -106,3 +86,9 @@ drop_evaluate(Key) ->
       Next = ets:next(?MODULE, Key),
       drop_evaluate(Next)
   end.
+
+handle_call(_Request, _From, _State) ->
+  erlang:error(not_implemented).
+
+handle_cast(_Request, _State) ->
+  erlang:error(not_implemented).
